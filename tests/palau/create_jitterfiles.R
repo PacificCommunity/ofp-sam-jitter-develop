ba601a1e5d13ea9ee50365bc590f9d004f8ff56c
Arni Magnusson <thisisarni@gmail.com>
2023-09-07 16:37:00 +1100

    Palau test

---
+++ tests/palau/create_jitterfiles.R
@@ -0,0 +1,29 @@
+## Jitter preparation
+
+library(FLR4MFCL)
+library(tools)  # file_path_sans_ext
+source("../../function/yft_2023_assessment/jitter.R")
+
+# Origin and destination
+model <- "alb_1_region"
+jitter.dir <- file.path("../../parfiles", model)
+sigma <- 0.100
+njitter <- 20
+
+# Read par file
+cat("* Reading parfile ... ")
+par <- read.MFCLPar(finalPar(jitter.dir))
+cat("done\n\n")
+
+# Crash here if jitter() doesn't run
+test <- jitter(par, 0.1, 1)
+
+# Write jittered par files into local dir
+suffix <- formatC(seq_len(njitter), width=nchar(njitter), flag="0")
+suffix <- paste0("_", suffix, ".par")
+for(i in 1:njitter)
+{
+  cat("Creating jitter file", i, "\n")
+  outfile <- paste0(file_path_sans_ext(finalPar(jitter.dir)), suffix[i])
+  write(jitter(par, sigma, i), outfile)
+}
