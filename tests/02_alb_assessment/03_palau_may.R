ff520310fa0e63ad45d2810e3287e284a5d9364a
Arni Magnusson <thisisarni@gmail.com>
2024-05-31 15:45:18 +1100

    Automate writing of test results

--- tests/02_alb_assessment/03_palau_may.R
+++ tests/02_alb_assessment/03_palau_may.R
@@ -18,12 +18,18 @@ cat("done\n\n")
 # Crash here if jitter() doesn't run
 test <- jitter(par, 0.1, 1)
 
-# Write jittered par files into local dir
+# Write jittered par files into results dir
 suffix <- formatC(seq_len(njitter), width=max(nchar(njitter), 2), flag="0")
 suffix <- paste0("_", suffix, ".par")
+outdir <- file.path("../../results", paste0(model, "_03_palau_may"))
+dir.create(outdir, showWarnings=FALSE)
 for(i in 1:njitter)
 {
   cat("Creating jitter file", i, "\n")
   outfile <- paste0(file_path_sans_ext(finalPar(jitter.dir)), suffix[i])
+  outfile <- file.path(outdir, basename(outfile))
   write(jitter(par, sigma, i), outfile)
 }
+
+# Copy input files into results dir
+file.copy(finalPar(jitter.dir), outdir, overwrite=TRUE)
